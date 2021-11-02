07fa220f5089f94378a7341c4471587cf9205934
Ahmed22 <Elkhami.it@gmail.com>
2021-11-03 00:44:25 +0200

    *Adding data binding values. *Handling data flow from xml to characters adapter and fragment. *Minor resources update.

--- app/src/main/java/com/elkhamitech/rickandmortycharacters/view/characters/CharactersAdapter.kt
+++ app/src/main/java/com/elkhamitech/rickandmortycharacters/view/characters/CharactersAdapter.kt
@@ -1,27 +1,44 @@
 package com.elkhamitech.rickandmortycharacters.view.characters
 
-import android.view.View
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.elkhamitech.rickandmortycharacters.data.model.Character
+import com.elkhamitech.rickandmortycharacters.databinding.ItemCharacterBinding
 
 /**
  * Created by A.Elkhami on 01,November,2021
  */
 class CharactersAdapter : ListAdapter<Character, CharactersAdapter.ViewHolder>(DiffUtilCallback()) {
 
-    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+    class ViewHolder private constructor(private val binding: ItemCharacterBinding) :
+        RecyclerView.ViewHolder(binding.root) {
 
+        fun bind(character: Character) {
+            binding.character = character
+        }
+
+        companion object {
+            fun from(parent: ViewGroup): ViewHolder {
+                val layoutInflater = LayoutInflater.from(parent.context)
+                val binding = ItemCharacterBinding.inflate(
+                    layoutInflater,
+                    parent, false
+                )
+                return ViewHolder(binding)
+            }
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        TODO("Not yet implemented")
+        return ViewHolder.from(parent)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        val character = getItem(position)
+        holder.bind(character)
     }
 
     private class DiffUtilCallback : DiffUtil.ItemCallback<Character>() {
--- app/src/main/java/com/elkhamitech/rickandmortycharacters/view/characters/CharactersFragment.kt
+++ app/src/main/java/com/elkhamitech/rickandmortycharacters/view/characters/CharactersFragment.kt
@@ -1,25 +1,32 @@
 package com.elkhamitech.rickandmortycharacters.view.characters
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
 import com.elkhamitech.rickandmortycharacters.R
+import com.elkhamitech.rickandmortycharacters.databinding.FragmentCharactersBinding
 
 class CharactersFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = CharactersFragment()
-    }
-
+    private lateinit var binding: FragmentCharactersBinding
     private lateinit var viewModel: CharactersViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_characters, container, false)
+    ): View {
+        binding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.fragment_characters,
+            container,
+            false
+        )
+        binding.viewModel = viewModel
+
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
